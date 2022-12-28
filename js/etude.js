428e22e3f00eeee6388d680e9995a33653757c85
emilyyujeong <118698609+emilyyujeong@users.noreply.github.com>
2022-12-28 17:39:35 +0900

    Add files via upload

--- js/etude.js
+++ js/etude.js
@@ -25,12 +25,30 @@ $(function () {
         autoplaySpeed: 0,
         variableWidth: true,
         cssEase: 'linear',
-        speed: 6000,
+        speed: 9000,
         centerMode: true,
         easing: 'linear'
 
     })
 
+    $('.wn_slide').slick({
+        arrows: false,
+        slidesToShow: 4,
+    });
+
+    $('.whatsnew .arrows i:nth-child(1)').on('click', function () {
+        $('.wn_slide').slick('slickPrev')
+    });
+    $('.whatsnew .arrows i:nth-child(2)').on('click', function () {
+        $('.wn_slide').slick('slickNext')
+    });
+
+
+    $('.sns_slide').slick({
+        arrows: false,
+        fade: true,
+
+    });
 
 
 })
